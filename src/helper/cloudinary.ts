c0809c4c4af1aa21d542a0a6867ebe49bd87b99e
Mohd Saif <iamg6102@gmail.com>
2025-12-26 16:32:28 +0530

    added

--- src/helper/cloudinary.ts
+++ src/helper/cloudinary.ts
@@ -9,7 +9,7 @@ cloudinary.config({
 });
 
 export async function uploadToCloudinary(file: File, folder?: string) {
-  const tempDir = "temp"
+  const tempDir = "tmp"
 
   if (!fs.existsSync(tempDir)) {
     fs.mkdirSync(tempDir, { recursive: true });
